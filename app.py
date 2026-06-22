f57613b07743f425da860de117c80a4c8988b537
Shree Nath Mahato <shreenath.ventures17@gmail.com>
2026-06-22 22:16:47 +0530

    Day 1 Completed

---
+++ app.py
@@ -0,0 +1,7 @@
+import streamlit as st
+
+st.set_page_config(page_title = 'VizML', layout = 'wide')
+st.title('Main')
+st.write('Welcome to VizML - Your multipage streamlit page')
+
+st.sidebar.success('Select a page from sidebar.')
---
+++ pages/page2.py
@@ -0,0 +1,4 @@
+import streamlit as st
+
+st.title("Page 2 - Visualization")
+st.write("This page will show charts and graphs.")
